ff05b62199ffe52bde07570c4b49ef5baac1bd7a
Agustin garcia <agustinchinchilla16@gmail.com>
2021-10-19 18:52:52 -0300

    Guardando cambio

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,7 +1,7 @@
 const Welcome = () => {
   return (
     <div className="container">
-      <h2 className="titleContainer">Welcome</h2>
+      <h2 className="titleContainer">Welcome!</h2>
       <lottie-player
         src="https://assets7.lottiefiles.com/packages/lf20_mrg9xhbm.json"
         background="transparent"
